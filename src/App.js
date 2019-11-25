99e161665ac23aa7d28a9f42edf07120b289289d
sobryan <stobryan@gmail.com>
2019-11-24 21:18:18 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MyFancyComponent from "./MyMapComponent"
+
+
+function App() {
+  return (
+    <div className="App">
+      <MyFancyComponent></MyFancyComponent>
+
+    </div>
+  );
+}
+
+export default App;
